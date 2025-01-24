25569f2b386d04a0e7cd4e2e2002fb427558630b
xihao <xihao@sunline.cn>
2025-01-24 10:30:35 +0800

    触发CI

--- .github/workflows/scripts/install-flutter.sh
+++ .github/workflows/scripts/install-flutter.sh
@@ -2,5 +2,5 @@
 
 BRANCH=$1
 
-git clone https://gitee.com/harmonycommando_flutter/flutter.git --depth 1 -b $BRANCH _flutter
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable _flutter
 echo "$GITHUB_WORKSPACE/_flutter/bin" >> $GITHUB_PATH
